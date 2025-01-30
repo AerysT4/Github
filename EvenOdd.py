c1231e606d10b63a04bf12f07ef3528b97576919
AerysT4 <jodaryat@gmail.com>
2025-01-30 18:00:35 +0530

    ss

---
+++ EvenOdd.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+def EvenOdd(num):
+    if num % 2 == 0:
+        print(f"{num} is an even number.")
+    else:
+        print(f"{num} is an odd number.")
+
+EvenOdd(num1)
+EvenOdd(num2)
